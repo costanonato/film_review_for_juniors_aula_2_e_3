98c5654e0971e1c7a701a63d8f9e1d6c8e0e0423
Nonato <nonato.web@gmail.com>
2024-01-31 20:46:07 -0300

    Add Pagination

--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpRequest
 
 from .models import Review
 
 
-def review_list(request):
+def review_list(request: HttpRequest):
     reviews = Review.published.all()
-    return render(request, "reviews/list.html", {"reviews": reviews})
+    paginator = Paginator(reviews, 2)
+    page_number = request.GET.get("page", 1)
+
+    try:
+        reviews_page = paginator.page(page_number)
+    except (EmptyPage, PageNotAnInteger):
+        reviews_page = paginator.page(1)
+
+    return render(request, "reviews/list.html", {"reviews_page": reviews_page})
 
 
 def review_detail(request, year, month, day, slugified_title):
